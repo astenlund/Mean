4b48559be60cc2518b4fdb7692d114f804ffb465
Andreas Stenlund (asd) <andreas.stenlund@tobii.com>
2014-02-27 10:57:59 +0100

    Initial add

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Mean
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var sum = 0;
+            var denom = 0;
+
+            foreach (var s in args)
+            {
+                int term;
+
+                if (int.TryParse(s, out term))
+                {
+                    sum += term;
+                    ++denom;
+                }
+            }
+
+            if (denom > 0)
+            {
+                var mean = sum/denom;
+
+                Clipboard.SetText(mean.ToString());
+                Console.WriteLine(mean);
+            }
+        }
+    }
+}
